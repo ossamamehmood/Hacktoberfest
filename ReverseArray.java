d7858717b342ab3dc5e812283dbce48f3fa0273c
Shikhar9425 <72194627+Shikhar9425@users.noreply.github.com>
2022-10-01 15:16:12 +0530

    Create ReverseArray.java
    print the elements of the array in reverse order

---
+++ ReverseArray.java
@@ -0,0 +1,16 @@
+public class ReverseArray {  
+    public static void main(String[] args) {  
+        //Initialize array  
+        int [] arr = new int [] {1, 2, 3, 4, 5};  
+        System.out.println("Original array: ");  
+        for (int i = 0; i < arr.length; i++) {  
+            System.out.print(arr[i] + " ");  
+        }  
+        System.out.println();  
+        System.out.println("Array in reverse order: ");  
+        //Loop through the array in reverse order  
+        for (int i = arr.length-1; i >= 0; i--) {  
+            System.out.print(arr[i] + " ");  
+        }  
+    }  
+}  
