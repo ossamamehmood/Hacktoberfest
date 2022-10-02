8b315e68b2cf8797312910c3793cde34ad66a03a
ossamamehmood <ossamamehmood110@gmail.com>
2022-10-02 17:09:25 +0500

    Hacktoberfest

--- Add Code Here/JAVA/ReverseLinkedList.java
+++ Add Code Here/JAVA/ReverseLinkedList.java
@@ -1,44 +1,62 @@
 import java.util.*;
 
-public class ReverseLinkedList 
-{
-        public static LinkedList<Integer> reverseLinkedList(LinkedList<Integer> MyLinkedList)
-	      {
-		        for (int i = 0; i < MyLinkedList.size() / 2; i++) 
-		        {
-			          Integer temp = MyLinkedList.get(i);
-			          MyLinkedList.set(i, MyLinkedList.get(MyLinkedList.size() - i - 1));
-			          MyLinkedList.set(MyLinkedList.size() - i - 1, temp);
-		        }
-		      return MyLinkedList;
-	      }
-	      public static void main(String[] args)
-        {   
-            // Creates LinkedList object
-            LinkedList<Integer> MyLinkedList = new LinkedList<Integer>();
-
-            //Initiates Scanner
-            Scanner sc = new Scanner(System.in);
-
-            //Takes input of how many elements we want to insert initially
-            System.out.println("Enter the initial size of the linked list");
-            int initialSize = sc.nextInt();
-
-            //Inserts elements into LinkedList by taking elements as an input from user
-            for (int i = 0; i < initialSize; i++) 
-            {
-                    System.out.print("Enter element at index " + (i) + ": ");
-                    int element = sc.nextInt();
-                    MyLinkedList.add(new Integer(element));
-
+public class ReverseLinkedList {
+    public static Lcheck head = null;
+    public static class Lcheck {
+        int data;
+        Lcheck next;
+    }
+    public static void get(int value) {
+        Lcheck temp = new Lcheck();
+        temp.data = value;
+        temp.next = null;
+        if (head == null) {
+            head = temp;
+        }
+        else {
+            Lcheck last_check = head;
+            while (last_check.next != null) {
+                last_check = last_check.next;
             }
-            
-            System.out.println("Linked list before reversing: " + MyLinkedList);
-            
-            //calls reverseLinkedList function which returns reversed linkedList
-            //and will store them in original linkedList
-	          MyLinkedList = reverseLinkedList(MyLinkedList);
-	          System.out.println("Linked list after reversing: " + MyLinkedList);
-	    }
-	
+            last_check.next = temp;
+        }
+    }
+    public static void print(Lcheck temp) {
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void reverseLinkedlist() {
+
+        Stack<Lcheck> t = new Stack<>();
+        Lcheck temp = head;
+        while (temp.next != null) {
+            t.add(temp);
+            temp = temp.next;
+        }
+        head = temp;
+        while (!t.isEmpty()) {
+            temp.next = t.peek();
+            t.pop();
+            temp = temp.next;
+        }
+        temp.next = null;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter 5 elements  for Reverse linked list");
+        for (int i = 0; i < 5; i++) {
+            int e = in.nextInt();
+            get(e);
+        }
+        System.out.print("linked list Before Reverse\n");
+        print(head);
+        reverseLinkedlist();
+        System.out.print("\nlinked list After Reverse\n");
+        print(head);
+    }
 }
+
