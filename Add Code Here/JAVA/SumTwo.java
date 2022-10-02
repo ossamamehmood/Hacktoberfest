5d46723a990700bcb81296fd005dc22bffa4d432
Jillian Oestreicher <jillianoestreicher@Jillians-MacBook-Pro.local>
2022-10-02 15:43:17 -0400

    added jill

---
+++ Add Code Here/JAVA/SumTwo.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    
+    int num1 = 5, num2 = 15,sum;
+
+    
+    sum = num1+num2;
+
+    
+    System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
+  }
+}
