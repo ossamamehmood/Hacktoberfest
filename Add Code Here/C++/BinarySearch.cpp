d9f3cd26ac9db9d1db398fe17a404e1534a400d1
Nitin <91310284+nitin-pandita@users.noreply.github.com>
2022-10-03 06:33:10 +0530

    Searching algorithm

--- Add Code Here/C++/BinarySearch.cpp
+++ Add Code Here/C++/BinarySearch.cpp
@@ -1,82 +1,54 @@
-
-#include <iostream>
-
-using namespace std;
-
-
-int binary_search(int arr[], int s, int e, int key)
-{
-    while(s <= e)
-    {
-        int mid = (s + e)/2;
-        
-        if(arr[mid] == key)
-        {
-            return mid;
-        }
-        else if(arr[mid] > key)
-        {
-            return binary_search(arr,s,mid-1,key);
-        }
-        else if(arr[mid] < key)
-        {
-            return binary_search(arr,mid+1,e,key);
-        }
-    }
-    
-    return -1;
-}
-
-
-int binary_search_using_shift(int arr[], int n, int key)
-{
-    int s = 0;
-    int e = n-1;
-    
-    while(s <= e)
-    {
-        int mid = (s+e)/2;
-        
-        if(arr[mid] == key)
-        {
-            return mid;
-        }
-        
-        else if(arr[mid] > key)
-        {
-            e = mid-1;
-            
-        }
-        else
-        {
-            s = mid+1;
-        }
-        
-    }
-    
-    return -1;
-}
-
-
-int main()
-{
-    int arr[100];
-    int n;
-    cin >>n;
-    
-    for(int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    
-    int key;
-    cin >> key;
-    cout << "Index:  " << binary_search(arr,0,n-1,key) << endl;
-    cout << "Index using shift:  " << binary_search_using_shift(arr,n,key) << endl;
-
-    return 0;
-}
-
-
-
-
+#include <iostream>  
+#include <conio.h>  
+using namespace std;  
+int main ()  
+{  
+    // declaration of the variables and array  
+    int arr[100], st, mid, end, i, num, tgt;  
+      
+    cout << " Define the size of the array: " << endl;  
+    cin >> num; // get size  
+      
+    // enter only sorted array  
+        cout << " Enter the values in sorted array either ascending or descending order: " << endl;  
+    // use for loop to iterate values  
+    for (i = 0; i < num; i++)  
+    {  
+        cout << " arr [" << i << "] = ";  
+        cin >> arr[i];  
+    }  
+      
+    // initialize the starting and ending variable's values  
+    st = 0;  
+    end = num - 1; // size of array (num) - 1  
+      
+    // define the item or value to be search  
+    cout << " Define a value to be searched from sorted array: " << endl;  
+    cin >> tgt;  
+      
+    // use while loop to check 'st', should be less than equal to 'end'.  
+    while ( st <= end)  
+    {  
+        // get middle value by splitting into half  
+        mid = ( st + end ) / 2;  
+        /* if we get the target value at mid index, print the position and exit from the program. */  
+        if (arr[mid] == tgt)  
+        {  
+            cout << " Element is found at index " << (mid + 1);  
+            exit (0); // use for exit program the program  
+        }  
+        // check the value of target element is greater than the mid element' value  
+        else if ( tgt > arr[mid])  
+        {  
+            st = mid + 1; // set the new value for st variable  
+        }  
+          
+        // check the value of target element is less than the mid element' value  
+        else if ( tgt < arr[mid])  
+        {  
+            end = mid - 1; // set the new value for end variable  
+        }  
+    }  
+    cout << " Number is not found. " << endl;  
+    return 0;  
+}   
