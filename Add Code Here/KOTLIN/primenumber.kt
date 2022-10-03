d4576b4f4e00a0acc5be9ead56386e61efe70e79
Darain <darain.mukarram@iomechs.com>
2022-10-03 15:50:32 +0500

    Added  code in kotlin of prime number

---
+++ Add Code Here/KOTLIN/primenumber.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    val num = 29
+    var flag = false
+    for (i in 2..num / 2) {
+        // condition for nonprime number
+        if (num % i == 0) {
+            flag = true
+            break
+        }
+    }
+
+    if (!flag)
+        println("$num is a prime number.")
+    else
+        println("$num is not a prime number.")
+}
