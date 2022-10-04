4c37f9bb092d5b4077c3a2988ae0dd11bed05488
Nicolò Santilio <nicolo.santilio@outlook.com>
2022-10-04 16:54:58 +0200

    Update tic_tac_toe.gd

--- Add Code Here/GDSCRIPT/tic_tac_toe.gd
+++ Add Code Here/GDSCRIPT/tic_tac_toe.gd
@@ -61,16 +61,16 @@ func check_frame() -> bool:
 
 func check_rows() -> bool:
 	for i in grid[0].size():
-		if (grid[i][0] == grid[i][1] == grid[i][2]):
+		if (grid[i][0] == grid[i][1] == grid[i][2] != 0):
 			return true
 	return false
 
 func check_columns() -> bool:
 	for i in grid[0].size():
-		if (grid[0][i] == grid[1][i] == grid[2][i]):
+		if (grid[0][i] == grid[1][i] == grid[2][i] != 0):
 			return true
 	return false
 
 func check_diagonals() -> bool:
-	return (grid[0][0] == grid[1][1] == grind[2][2]) or (grid[0][2] == grid[1][1] == grid[2][0])
+	return (grid[0][0] == grid[1][1] == grind[2][2] != 0) or (grid[0][2] == grid[1][1] == grid[2][0] != 0)
   
