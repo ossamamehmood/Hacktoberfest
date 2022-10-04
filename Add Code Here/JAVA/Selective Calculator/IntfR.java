8e8906f42ad6effb1f94f1b4abfa6ee24044f8f4
Shourya Gupta <gptshourya02@gmail.com>
2022-10-04 23:49:24 +0530

    Selective Calculator Hey I have added a selective scientific and basic calculator functionality depending on the username in JAVA Such as if the username starts with A he can get access to only add and subtract, if starts with S then scientific calculator is accessible to them and so on please assign this task to me so that I can get my first PR of hacktoberfest 2022.

---
+++ Add Code Here/JAVA/Selective Calculator/IntfR.java
@@ -0,0 +1,8 @@
+interface IntfR {
+	double add(double a, double b);
+	double sub(double a, double b);
+	double mul(double a, double b);
+	double div(double a, double b);
+	double mod(double a, double b);
+	double exp(double a, double b);
+}
