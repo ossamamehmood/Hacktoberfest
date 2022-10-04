39c6867e3f2145789dc7d9e3d0d0b4877cb58311
Amul Rathore <71003626+amul-code@users.noreply.github.com>
2022-10-05 01:23:13 +0530

    kadane's algorithm using Python
    Kadane’s algorithm is one of the famous approaches to solve the problem using dynamic programming. As we all know, the maximum subarray problem is one of the famous problems in the field of dynamic programming.

---
+++ kadanes_algo.py
@@ -0,0 +1,17 @@
+def maxSubArraySum(arr,size):
+    
+    max_till_now = arr[0]
+    max_ending = 0
+    
+    for i in range(0, size):
+        max_ending = max_ending + arr[i]
+        if max_ending < 0:
+            max_ending = 0
+        elif (max_till_now < max_ending):
+            max_till_now = max_ending
+    return max_till_now
+
+#Driver Code
+arr = list(map(int,input().split(" ")))
+size = len(arr)
+print(maxSubArraySum(arr,size))
