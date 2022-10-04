d85923ba8970c03278184d9371265dfa4ac005e4
sahil2311sharma <sahilsharma123027@gmail.com>
2022-10-05 05:08:50 +0530

    added Pathsum Problem leetcode solution

---
+++ Add Code Here/C++/LeetCode-PathSum.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root) return false;
+        if(!root->left and !root->right and targetSum-root->val == 0)    return 1;
+        return (hasPathSum(root->left,targetSum-root->val)) or (hasPathSum(root->right,targetSum-root->val));
+    }
+};
