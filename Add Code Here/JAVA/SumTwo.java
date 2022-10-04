7ff7451ee643979927da5908b7892ebbc6be4850
Satyam Pandey <66619457+imsatyampandey@users.noreply.github.com>
2022-10-04 21:29:00 +0530

    Update SumTwo.java

--- Add Code Here/JAVA/SumTwo.java
+++ Add Code Here/JAVA/SumTwo.java
@@ -1,12 +1,12 @@
 public class Main {
   public static void main(String[] args) {
     
-    int num1 = 5, num2 = 15,sum;
+    int num1 = 5, num2 = 15;
+
+  
 
     
-    sum = num1+num2;
-
-    
-    System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
+    System.out.println("Sum of "+num1+" and "+num2+" is: "+(num1+num2));
   }
 }
+//by satyam 
