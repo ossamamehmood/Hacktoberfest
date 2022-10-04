e24568846c506b8824b1040eeabf7913c893b80a
Anant Chaudhary <78264061+anant0070@users.noreply.github.com>
2022-10-04 14:54:11 +0530

    Update bubble sort.cpp
    Taking input on runtime and calculating number of passes

--- Add Code Here/C++/bubble sort.cpp
+++ Add Code Here/C++/bubble sort.cpp
@@ -1,36 +1,39 @@
-// C++ program for implementation
-// of Bubble sort
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
- 
-// A function to implement bubble sort
-void bubbleSort(int arr[], int n)
-{
-    int i, j;
-    for (i = 0; i < n - 1; i++)
- 
-        // Last i elements are already
-        // in place
-        for (j = 0; j < n - i - 1; j++)
-            if (arr[j] > arr[j + 1])
-                swap(arr[j], arr[j + 1]);
-}
- 
-// Function to print an array
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-}
- 
-// Driver code
+
 int main()
 {
-    int arr[] = { 5, 1, 4, 2, 8};
-    int N = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, N);
-    cout << "Sorted array: \n";
-    printArray(arr, N);
+    int n;
+    cout<<"Enter size of array "<<endl;
+    cin>>n;
+    int arr[n],i,j,pass;
+    cout<<"Enter the elements"<<endl;
+
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Original Array : "<<endl;
+    for(i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+           if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+        pass++;
+    }
+    cout<<"New Array : "<<endl;
+    for(i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+    cout<<endl;
+    cout<<"Number of pass "<<pass;
     return 0;
+}
