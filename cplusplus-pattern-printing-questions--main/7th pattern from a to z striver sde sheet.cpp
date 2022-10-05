68390124a60822d9e7013e6509cc93b465b82ef1
Nidhi1k <88227668+Nidhi1k@users.noreply.github.com>
2022-10-05 17:09:48 +0530

    Add files via upload

---
+++ cplusplus-pattern-printing-questions--main/7th pattern from a to z striver sde sheet.cpp
@@ -0,0 +1,26 @@
+//7th pattern from a to z striver sde sheet
+//upward pyramid
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int n = 5;
+	// your code goes here
+	//use two for loops outside one will iterate for each row and inside for loop will iterate foe each column 
+	for(int i = 1;i<=n ;i++)
+	{//we need some if else in this case 
+	   	for(int j =1;j<=2*n-1 ;j++)
+    	{   
+    	    if(j>=n-(i-1)&&j<=n+(i-1))
+    	    {
+    	        cout<<"*";
+    	    }
+    	    else 
+    	    cout<<" ";
+    	    
+    	} 
+    	//endl for new line after each row completes printing star
+    	cout<<endl;
+	}
+	return 0;
+}
