9d339b934d04f461e9c3d3a52603e401efb84bc2
Sandeep Neeli <rebcasandy07@gmail.com>
2022-10-05 18:28:55 +0530

    Added BinarySearch Resurvive

--- Add Code Here/C++/BinarySearchRec.cpp
+++ Add Code Here/C++/BinarySearchRec.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 
 int binarySearch(int array[], int x, int low, int high) {
-  
-	// Repeat until the pointers low and high meet each other
-  while (low <= high) {
+  if (high >= low) {
     int mid = low + (high - low) / 2;
 
+    // If found at mid, then return it
     if (array[mid] == x)
       return mid;
 
-    if (array[mid] < x)
-      low = mid + 1;
+    // Search the left half
+    if (array[mid] > x)
+      return binarySearch(array, x, low, mid - 1);
 
-    else
-      high = mid - 1;
+    // Search the right half
+    return binarySearch(array, x, mid + 1, high);
   }
 
   return -1;
