2776774fe2b8f990554ff7b002b86215e2d2acef
Priyanshuiiitg <98309398+Priyanshuiiitg@users.noreply.github.com>
2022-10-05 21:25:07 +0530

    maximum_subarray_divide_and_conquer.cpp

--- Add Code Here/C++/Kadane's_Algorithm.cpp
+++ Add Code Here/C++/Kadane's_Algorithm.cpp
@@ -1,29 +1,39 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-int maxSubArraySum(int a[], int size)
-{
-    int max_so_far = INT_MIN, max_ending_here = 0;
- 
-    for (int i = 0; i < size; i++) {
-        max_ending_here = max_ending_here + a[i];
-        if (max_so_far < max_ending_here)
-            max_so_far = max_ending_here;
- 
-        if (max_ending_here < 0)
-            max_ending_here = 0;
+class Solution {
+public:
+    int msd(vector<int> &v,int left,int right )
+    {
+        int mid=(left+right)/2;
+        
+        if(left==right)
+            return v[left];
+        int leftans=msd(v,left,mid);
+        int rightans=msd(v,mid+1,right);
+        int temp=0;
+        int leftmax=v[mid];
+        int rightmax=v[mid+1];
+        for(int i=mid;i>=left;i--)
+        {
+            temp+=v[i];
+            if(temp>leftmax)
+                leftmax=temp;
+            
+        }
+        temp=0;
+     for(int i=mid+1;i<=right;i++)
+        {
+            temp+=v[i];
+            if(temp>rightmax)
+                rightmax=temp;
+            
+        }
+       return max((leftans,rightans),leftmax+rightmax);
+        
+        
+        
     }
-    return max_so_far;
-}
- 
-// Driver Code
-int main()
-{
-    int a[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
-    int n = sizeof(a) / sizeof(a[0]);
- 
-    // Function Call
-    int max_sum = maxSubArraySum(a, n);
-    cout << "Maximum contiguous sum is " << max_sum;
-    return 0;
-}
+    int maxSubArray(vector<int>& nums) {
+        return msd(nums,0,nums.size()-1);
+        
+        
+    }
+};
