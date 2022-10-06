6aa869532be60b60ecc97abed69d6c080925b05c
Soujanya <soujanyatodkar@gmail.com>
2022-10-06 18:47:13 +0530

    added myself

--- Add Code Here/C++/pathsum.cpp
+++ Add Code Here/C++/pathsum.cpp
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/path-sum/
+// https://leetcode.com/problems/path-sum/
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
---
+++ content/participant/soujanya-08.md
@@ -0,0 +1,5 @@
+---
+name: Soujanya
+institution: KLE Institute Of Technology
+github:soujanya-08
+---
