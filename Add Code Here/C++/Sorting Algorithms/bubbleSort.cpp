d7f6fa0a688e1b23ab926573ccb0408245d573fb
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2022-10-07 21:25:30 +0500

    Updated Bubblesort.cpp

--- Add Code Here/C++/Sorting Algorithms/bubbleSort.cpp
+++ Add Code Here/C++/Sorting Algorithms/bubbleSort.cpp
@@ -4,23 +4,21 @@ using namespace std;
 
 // BUBBLE SORT
 //------------------------------------------------------- 
-
+// ------------ sorting in ascending order 
 void bubbleSort(vector <int> &v, int n) {
-	bool swapped = true;
 	int i = 0;
 
-	while (i < n - 1 && swapped) { // keep going while we swap in the unordered part
-		swapped = false;
+	for(int i=0;i<n-1;i++{ 
 		
-		for (int j = n - 1; j > i; j--) { // unordered part
+		for (int j = 0; j < n-i-1 ; j++) { // unordered part
 			
-			if (v[j] < v[j - 1]) {
-				swap(v[j], v[j - 1]);
-				swapped = true;
+			if (v[j] > v[j + 1]) {
+				swap(v[j], v[j + 1]); 
 			}
 		}
-		i++;
 	}
+	    
 }
+	   
 
 
