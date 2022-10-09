a1dba897c66d59a97fb28ad25a336084e86d75e1
Amit Pawar <110733735+Amitpawar88@users.noreply.github.com>
2022-10-08 23:24:14 -0700

    Update recursion_subsequence.java

--- Add Code Here/JAVA/Recursion/recursion_subsequence.java
+++ Add Code Here/JAVA/Recursion/recursion_subsequence.java
@@ -1,17 +1,53 @@
+import java.util.ArrayList;
+
 public class recursion_subsequence {
-    public static void printSubsequence(String str , int index, String newString){
-        if(index == str.length()){
-            System.out.println(newString);
+    static void subseq(String p, String up) {
+        if (up.isEmpty()) { 
+            System.out.println(p);
             return;
         }
-        char currentChar = str.charAt(index);
-        // to add
-        printSubsequence(str, index+1, newString+currentChar);
-        //not to add
-        printSubsequence(str, index+1, newString);
+
+        char ch = up.charAt(0);
+
+        subseq(p + ch, up.substring(1));
+        subseq(p, up.substring(1));
+
+    }
+
+    static ArrayList<String> subseqRet(String p, String up) {
+        if (up.isEmpty()) { 
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> left = subseqRet(p + ch, up.substring(1));
+        ArrayList<String> right = subseqRet(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
+
+    }
+
+    static void subseqAscii(String p, String up) {
+        if (up.isEmpty()) { 
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        subseqAscii(p + ch, up.substring(1));
+        subseqAscii(p, up.substring(1));
+        subseqAscii(p + (ch + 0), up.substring(1));
+
     }
     public static void main(String[] args) {
-        String str = "abc";
-        printSubsequence(str, 0, "");
-    }
+       subseq("", "abc");
+       System.out.println(subseqRet("", "abc"));
+       subseqAscii("", "abc");
+        
+    } 
 }
