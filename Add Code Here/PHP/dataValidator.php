4215c636d476210988b5c6d0dd7adac4ba98ef55
chandachewe10 <chewec03@gmail.com>
2022-10-11 10:24:17 +0100

    Added Validation by Object Method

--- Add Code Here/PHP/dataValidator.php
+++ Add Code Here/PHP/dataValidator.php
@@ -1,45 +1,66 @@
 <?php
-class validator{
-    public function email($entered_email){
-
-        if (!filter_var($entered_email, FILTER_VALIDATE_EMAIL)) {
-            $emailErr = "Invalid email address";
-          }
-    }
-
-    public function url($entered_url) {
-        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$entered_url)) {
-            $websiteErr = "Invalid URL";
-          }
-    }
-
-    public function phone_number_zm_format($phone_number_zm_format) {
-        if (!preg_match('/^(09|07)[5|6|7][0-9]{7}$/',$phone_number_zm_format)){
-            $phoneError = "Your phone number is invalid. Your phone must begin with either 096,097,095,076,077, or 075";
+$Error = array();
+if ($_SERVER['REQUEST_METHOD']=="POST") {
+    class validator
+    {
+        public function email($entered_email)
+        {
+            if (!filter_var($entered_email, FILTER_VALIDATE_EMAIL)) {
+                $Error = "Invalid email address";
+                return $Error;
             }
-    }
+        }
 
-    public function nrc_zm_format($nrc_zm_format) {
-        if (!preg_match('/^(\d{6})\/\d{2}\/\d{1}$/',$nrc_zm_format)){
-            $nrcError = "Your NRC is invalid. Your NRC must be in this format ******/**/*";
+        public function url($entered_url)
+        {
+            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $entered_url)) {
+                $Error = "Invalid URL";
+                return $Error;
             }
-    }
+        }
 
-    public function first_name_only($firstName) {
-        if (!preg_match('/^[a-zA-Z]{2,50}$/',$firstName)){
-            $firstNameError = "Your name is invalid. Ensure your first name does not contain spaces";
+        public function phone_number_zm_format($phone_number_zm_format)
+        {
+            if (!preg_match('/^(09|07)[5|6|7][0-9]{7}$/', $phone_number_zm_format)) {
+                $Error = "Your phone number is invalid. Your phone must begin with either 096,097,095,076,077, or 075";
+           return $Error;
             }
-    }
+        }
 
-    public function full_name_only($fullName) {
-        if (!preg_match('/([a-zA-Z]){2,50}\s([a-zA-Z]){2,50}$/',$fullName)){
-            $fullNameError = "Your Full Name is invalid";
+        public function nrc_zm_format($nrc_zm_format)
+        {
+            if (!preg_match('/^(\d{6})\/\d{2}\/\d{1}$/', $nrc_zm_format)) {
+                $Error = "Your NRC is invalid. Your NRC must be in this format ******/**/*";
+                return $Error;
             }
+        }
+
+        public function first_name_only($firstName)
+        {
+            if (!preg_match('/^[a-zA-Z]{2,50}$/', $firstName)) {
+                $Error = "Your name is invalid. Ensure your first name does not contain spaces";
+                return $Error; 
+            }
+        }
+
+        public function full_name_only($fullName)
+        {
+            if (!preg_match('/([a-zA-Z]){2,50}\s([a-zA-Z]){2,50}$/', $fullName)) {
+                $Error = "Your Full Name is invalid";
+                return $Error;
+            }
+        }
     }
+     
+    $validate = new validator();
+    $EmailErr = $validate->email($_POST['email']);
+    $UrlErr = $validate->url($_POST['url']);
+    $PhoneErr = $validate->phone_number_zm_format($_POST['phone']);
+    
+    $Error = array($EmailErr,$UrlErr,$PhoneErr);
+   
+    
 }
-
-
-
 
 ?>
 
@@ -108,13 +129,34 @@ form{
         
   <form action="<?PHP echo ($_SERVER['PHP_SELF']); ?>";  method="POST">
     <h3>TESTING VALIDATION</h3>
+
+    
+
     <h4>INPUTS</h4>
-        <input type="number" name="email" id="name" placeholder="Enter Email" ></br>
+    <!--Show Errors here -->
+    
+  
+  <?php  foreach ($Error as $Errors){?>
+    <ul> 
+<li><span class="error" style="color:red"> <?php echo $Errors;?></span></li>
+  </ul>
+    
+   <?php
+} 
+?>
+   
+    <!--End Showing Errors--> 
+
+
+        <input type="text" name="email" id="name" placeholder="Enter Email" ></br>
+        
         <input type="text" name="url" id="name" placeholder="Enter URL" ></br>
         <input type="number" name="phone" id="name" placeholder="Enter phone number"></br>
+        <!--
         <input type="text" name="nrc" id="name" placeholder="Enter nrc"></br>
         <input type="text" name="firstname" id="name" placeholder="Enter first name"></br>
         <input type="text" name="fullname" id="name" placeholder="Enter full name"></br>
+    -->
         <input type="submit" value="submit" id="upload">
         <br><br>
         
