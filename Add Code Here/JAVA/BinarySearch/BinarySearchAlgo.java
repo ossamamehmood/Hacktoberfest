dbd61386445099af3e48b4e9dd16b1a13197b66c
MOHD AIMAN SALEEM <81529934+mohdaimansaleem@users.noreply.github.com>
2022-10-12 19:39:52 +0530

    Fixed an edge case

--- Add Code Here/JAVA/BinarySearch/BinarySearchAlgo.java
+++ Add Code Here/JAVA/BinarySearch/BinarySearchAlgo.java
@@ -1,13 +1,15 @@
 import java.util.*;
 public class BinarySearch { 
-
+    
     public static int binarySearch(int[] arr, int x) {
     	//Your code goes here
         int start = 0;
         int end  = arr.length-1;
         
         while(start <= end){
-            int mid = (start+end)/2;
+            
+            //To avoid integer overflow
+            int mid = start+(end-start)/2;
             if(arr[mid] == x){
                 return mid;
             }else if(arr[mid] > x){
