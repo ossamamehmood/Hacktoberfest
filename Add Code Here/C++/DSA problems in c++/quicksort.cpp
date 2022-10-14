08a8d9bf9ee12d29be30d11c05dd6231e104e705
Ankit Adhikari <ankitad733@gmail.com>
2022-10-14 09:51:41 +0530

    quicksort.cpp

--- Add Code Here/C++/DSA problems in c++/quicksort.cpp
+++ Add Code Here/C++/DSA problems in c++/quicksort.cpp
@@ -1,74 +1,65 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <queue>
+#include <stack>
+#include <map>
+
 using namespace std;
-
-int partition(int arr[], int s, int e)
-{
-    int pivot = arr[s];
-    int cnt = 0;
-
-    for(int i = s+1; i <= e; i++)
-    {
-        if(arr[i] <= pivot)
-            cnt++;
-
-    }
-
-    int pivotIndex = s + cnt;
-    swap(arr[pivotIndex], arr[s]);
-
-    //handle aleft & right part
-
-    int i = s, j = e;
-
-    while(i < pivotIndex && j > pivotIndex)
-    {
-        while(arr[i] < pivot)
-        {
-            i++;
-        }
-
-        while(arr[j] > pivot)
-        {
-            j--;
-        }
-
-        if(i < pivotIndex && j > pivotIndex)
-        {
-            swap(arr[i++], arr[j--]);
-        }
-
-        return pivotIndex;
-    }
-}
-
-void quicksort(int arr[], int s, int e)
-{
-    // base case
-    if (s >= e)
-        return;
-
-    // parttion
-    int p = partition(arr, s, e);
-
-    // left part sort
-    quicksort(arr, s, p-1);
-
-    //right part sort
-    quicksort(arr, p+1, e);
-}
 
 int main()
 {
-    int arr[5] = {2, 4, 1, 6, 8};
-    int n = 5;
-
-    quicksort(arr, 0, n - 1);
-
-    for (int i = 0; i < n; i++)
+    int n;
+    cin>>n;
+    int a[n][n];
+    for(int i=0;i<n;i++)
     {
-        cout << arr[i] << " ";
+        for(int j=0;j<n;j++)
+        {
+            cin>>a[i][j];
+        }
     }
-    cout << endl;
-
+    int k=0;
+    int l=0;
+    int m=n-1;
+    int p=n-1;
+    while(k<=m && l<=p)
+    {
+        for(int i=l;i<=p;i++)
+        {
+            cout<<a[k][i]<<" ";
+        }
+        k++;
+        for(int i=k;i<=m;i++)
+        {
+            cout<<a[i][p]<<" ";
+        }
+        p--;
+        if(k<=m)
+        {
+            for(int i=p;i>=l;i--)
+            {
+                cout<<a[m][i]<<" ";
+            }
+            m--;
+        }
+        if(l<=p)
+        {
+            for(int i=m;i>=k;i--)
+            {
+                cout<<a[i][l]<<" ";
+            }
+            l++;
+        }
+    }
     return 0;
 }
