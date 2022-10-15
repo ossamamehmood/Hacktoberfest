1522cbc34275dbee45fa6d81b66d4571a3a76e0c
Ravin Wijekoon <50649031+ravinWijekoon@users.noreply.github.com>
2022-10-15 11:22:58 +0530

    Added Website Availability Checker
    It gives an output whether the input website is available or not

---
+++ Add Code Here/PYTHON/WebsiteAvailabilityChecker.py
@@ -0,0 +1,9 @@
+import requests
+
+#200 is the success status code of requests there fore it checks whether 200 is there.
+try:
+    request = requests.get('http://www.sliit123.lk')
+    if request.status_code == 200:
+        print('Web site exists')
+except:
+    print("Website does not exists")
