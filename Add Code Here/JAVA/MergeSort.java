91dc29ed4de1a2a1b4de83d4ee2668199fbdd80d
ossamamehmood <ossamamehmood110@gmail.com>
2022-10-18 03:15:13 +0500

    Hacktoberfest

--- Add Code Here/JAVA/MergeSort.java
+++ Add Code Here/JAVA/MergeSort.java
@@ -1,5 +1,6 @@
 /* Java program for Merge Sort */
-class MergeSort {
+class MergeSort
+{
 	// Merges two subarrays of arr[].
 	// First subarray is arr[l..m]
 	// Second subarray is arr[m+1..r]
@@ -10,14 +11,15 @@ class MergeSort {
 		int n2 = r - m;
 
 		/* Create temp arrays */
-		int L[] = new int[n1];
-		int R[] = new int[n2];
+		int L[] = new int [n1];
+		int R[] = new int [n2];
 
 		/*Copy data to temp arrays*/
-		for (int i = 0; i < n1; ++i)
+		for (int i=0; i<n1; ++i)
 			L[i] = arr[l + i];
-		for (int j = 0; j < n2; ++j)
-			R[j] = arr[m + 1 + j];
+		for (int j=0; j<n2; ++j)
+			R[j] = arr[m + 1+ j];
+
 
 		/* Merge the temp arrays */
 
@@ -26,12 +28,15 @@ class MergeSort {
 
 		// Initial index of merged subarray array
 		int k = l;
-		while (i < n1 && j < n2) {
-			if (L[i] <= R[j]) {
+		while (i < n1 && j < n2)
+		{
+			if (L[i] <= R[j])
+			{
 				arr[k] = L[i];
 				i++;
 			}
-			else {
+			else
+			{
 				arr[k] = R[j];
 				j++;
 			}
@@ -39,14 +44,16 @@ class MergeSort {
 		}
 
 		/* Copy remaining elements of L[] if any */
-		while (i < n1) {
+		while (i < n1)
+		{
 			arr[k] = L[i];
 			i++;
 			k++;
 		}
 
 		/* Copy remaining elements of R[] if any */
-		while (j < n2) {
+		while (j < n2)
+		{
 			arr[k] = R[j];
 			j++;
 			k++;
@@ -57,13 +64,14 @@ class MergeSort {
 	// merge()
 	void sort(int arr[], int l, int r)
 	{
-		if (l < r) {
+		if (l < r)
+		{
 			// Find the middle point
-			int m = l + (r - l) / 2;
+			int m = (l+r)/2;
 
 			// Sort first and second halves
 			sort(arr, l, m);
-			sort(arr, m + 1, r);
+			sort(arr , m+1, r);
 
 			// Merge the sorted halves
 			merge(arr, l, m, r);
@@ -74,24 +82,24 @@ class MergeSort {
 	static void printArray(int arr[])
 	{
 		int n = arr.length;
-		for (int i = 0; i < n; ++i)
+		for (int i=0; i<n; ++i)
 			System.out.print(arr[i] + " ");
 		System.out.println();
 	}
 
-	// Driver code
+	// Driver method
 	public static void main(String args[])
 	{
-		int arr[] = { 12, 11, 13, 5, 6, 7 };
+		int arr[] = {12, 11, 13, 5, 6, 7};
 
 		System.out.println("Given Array");
 		printArray(arr);
 
 		MergeSort ob = new MergeSort();
-		ob.sort(arr, 0, arr.length - 1);
+		ob.sort(arr, 0, arr.length-1);
 
 		System.out.println("\nSorted array");
 		printArray(arr);
 	}
 }
-/* This code is contributed by Rajat Mishra */
+/* This code is contributed by Unknown */
