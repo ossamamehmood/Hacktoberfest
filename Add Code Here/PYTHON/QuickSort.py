5d9733b2735d714a161eace0db7536f90313b5cd
Kavindu Chethani <46352484+kavindu-chethani@users.noreply.github.com>
2022-10-20 01:44:33 +0530

    beginning for algorithms

--- Add Code Here/PYTHON/QuickSort.py
+++ Add Code Here/PYTHON/QuickSort.py
@@ -1,26 +1,32 @@
-def quicksort(a):
-    if len(a)<=1:
-        return a
+def QuickSort(arr):
 
-    else:
-        pivot = a.pop()
+    elements = len(arr)
+  
+    if elements < 2:
+        return arr
+    
+    current_position = 0 
 
-    greater =[]
-    lower=[]
+    for i in range(1, elements): 
+         if arr[i] <= arr[0]:
+              current_position += 1
+              temp = arr[i]
+              arr[i] = arr[current_position]
+              arr[current_position] = temp
 
-    for i in a:
-        if i > pivot:
-            greater.append(i)
-        else:
-            lower.append(i)    
+    temp = arr[0]
+    arr[0] = arr[current_position] 
+    arr[current_position] = temp 
+    
+    left = QuickSort(arr[0:current_position]) 
+    right = QuickSort(arr[current_position+1:elements]) 
 
-    return quicksort(lower) + [pivot] + quicksort(greater)
-
-
-arr=[56,3,4,2,6,3,7]
-
-print(quicksort(arr))
+    arr = left + [arr[current_position]] + right 
+    
+    return arr
 
 
 
-
+array_to_be_sorted = [4,2,7,3,1,6]
+print("Original Array: ",array_to_be_sorted)
+print("Sorted Array: ",QuickSort(array_to_be_sorted))
