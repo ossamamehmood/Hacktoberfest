435bedfc8116e8a1e428d9e445064921234cade3
Preeti019 <116053030+Preeti019@users.noreply.github.com>
2022-10-19 13:33:21 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+# get vs [] for retrieving elements
+my_dict = {'name': 'Jack', 'age': 26}
+
+# Output: Jack
+print(my_dict['name'])
+
+# Output: 26
+print(my_dict.get('age'))
+
+# Trying to access keys which doesn't exist throws error
+# Output None
+print(my_dict.get('address'))
+
+# KeyError
+print(my_dict['address'])
