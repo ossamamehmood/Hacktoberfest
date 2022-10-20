b0679217d4a4d91b80320c16c72cae1717c81b26
Srishti Chauhan <104318680+Srishtichauhan5359@users.noreply.github.com>
2022-10-20 18:02:43 +0530

    Reverse-Linked-List-problem

---
+++ Add Code Here/PYTHON/Basic Pyhton Programs/Reverse-Linked-List-problem.py
@@ -0,0 +1,69 @@
+#Reverse Linked List problem 
+#Hackerrank
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+def reverse(head):
+    nxt = None
+    while head:
+        tmp = head.next
+        head.next = nxt
+        nxt = head
+        head = tmp
+    return nxt
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
