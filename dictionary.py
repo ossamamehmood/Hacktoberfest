ff528913ca36414cc89077bee9a192d58e5931c6
Diya Agrawal <agrawaldiya32@gmail.com>
2022-10-22 01:48:50 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+# Creating a Dictionary
+# with Integer Keys
+Dict = {1: 'Doing', 2: 'For', 3: 'Change'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+
+# Creating a Dictionary
+# with Mixed keys
+Dict = {'Name': 'Something', 1: [1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
