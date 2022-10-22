a8e4819f178faa30af36c8b1572418db18f08de2
ayush91985 <67649316+ayush91985@users.noreply.github.com>
2022-10-23 01:01:27 +0530

    added

---
+++ Add Code Here/C++/Detect loop in linked list_ayush91985.cpp
@@ -0,0 +1,70 @@
+// C++ program to detect loop in a linked list
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node* next;
+};
+
+void push(struct Node** head_ref, int new_data)
+{
+	/* allocate node */
+	struct Node* new_node = new Node;
+
+	/* put in the data */
+	new_node->data = new_data;
+
+	/* link the old list off the new node */
+	new_node->next = (*head_ref);
+
+	/* move the head to point to the new node */
+	(*head_ref) = new_node;
+}
+
+// Returns true if there is a loop in linked list
+// else returns false.
+bool detectLoop(struct Node* h)
+{
+	unordered_set<Node*> s;
+	while (h != NULL) {
+		// If this node is already present
+		// in hashmap it means there is a cycle
+		// (Because you will be encountering the
+		// node for the second time).
+		if (s.find(h) != s.end())
+			return true;
+
+		// If we are seeing the node for
+		// the first time, insert it in hash
+		s.insert(h);
+
+		h = h->next;
+	}
+
+	return false;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+	/* Start with the empty list */
+	struct Node* head = NULL;
+
+	push(&head, 20);
+	push(&head, 4);
+	push(&head, 15);
+	push(&head, 10);
+
+	/* Create a loop for testing */
+	head->next->next->next->next = head;
+
+	if (detectLoop(head))
+		cout << "Loop found";
+	else
+		cout << "No Loop";
+
+	return 0;
+}
+// This code is contributed by Geetanjali
