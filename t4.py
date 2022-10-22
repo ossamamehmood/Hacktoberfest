b16523d425faf541909b23066f7708abc6cb430c
ossamamehmood <ossamamehmood110@gmail.com>
2022-10-22 19:57:44 +0500

    Hacktoberfest

--- t4.py
+++
@@ -1,8 +0,0 @@
-#program to create star shape
-from turtle import * 
-right(75)
-forward(100)
-for i in range(4):
-    right(144)
-    forward(100)   
-done()
--- t5.py
+++
@@ -1,9 +0,0 @@
-#program to draw a polygon
-from turtle import *
-num_sides = 6
-side_length = 70
-angle = 360.0 / num_sides
-for i in range(num_sides):
-  forward(side_length)
-  right(angle)
-done()
