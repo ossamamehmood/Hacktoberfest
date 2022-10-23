e191ab819fddf21046181fd5dedca46e9995fa29
talhamasood0000 <64506978+talhamasood0000@users.noreply.github.com>
2022-10-24 00:59:01 +0500

    Create recursive-factorial.py

---
+++ Add Code Here/PYTHON/recursive-factorial.py
@@ -0,0 +1,17 @@
+# Factorial of a number using recursion
+
+def recur_factorial(n):
+   if n == 1:
+       return n
+   else:
+       return n*recur_factorial(n-1)
+
+num = 7
+
+# check if the number is negative
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   print("The factorial of", num, "is", recur_factorial(num))
