c47acbe5d31f250f60e436400d038bd4ace1ddfb
AmareshSM <87269971+AmareshMuddebihal@users.noreply.github.com>
2022-10-23 10:21:00 +0530

    Create creation_List.py

---
+++ Add Code Here/creation_List.py
@@ -0,0 +1,17 @@
+
+# Creating a List
+List = []
+print("Blank List: ")
+print(List)
+
+# Creating a List of numbers
+List = [10, 20, 14]
+print("\nList of numbers: ")
+print(List)
+
+# Creating a List of strings and accessing
+# using index
+List = [" A ", " B ", "  c  "]
+print("\nList Items: ")
+print(List[0])
+print(List[2])
