66fac143f267042c26850e7e1c77a1ff5615a203
DEEP NARAYAN BHAGAT <74817931+deepakbhagat7@users.noreply.github.com>
2022-10-23 22:27:02 +0530

    Create InplaceMergeSort.java

---
+++ Add Code Here/JAVA/InplaceMergeSort.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+
+public class MergeSortInPlace {
+
+	public static void main(String[] args) {
+		  int[] arr = {5, 4, 3, 2, 1};
+	        mergeSortInPlace(arr, 0, arr.length);
+	        System.out.println(Arrays.toString(arr));
+	    }
+	
+
+    static void mergeSortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+
+        int mid = (s + e) / 2;
+
+        mergeSortInPlace(arr, s, mid);
+        mergeSortInPlace(arr, mid, e);
+
+        mergeInPlace(arr, s, mid, e);
+    }
+
+    private static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] mix = new int[e - s];
+
+        int i = s;
+        int j = m;
+        int k = 0;
+
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while (i < m) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int l = 0; l < mix.length; l++) {
+            arr[s+l] = mix[l];
+        }
+    }
+
+}
