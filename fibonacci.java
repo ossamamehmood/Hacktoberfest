9242aac0f3f20b7d6662acc871fe9d83c6bb64a6
Akshat Bokdia <101403537+AkshatBokdia0602@users.noreply.github.com>
2022-10-25 11:07:28 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,17 +1,47 @@
-class Fibonacci {
-  public static void main(String[] args) {
+import java.util.*;
 
-    int n = 100, firstTerm = 0, secondTerm = 1;
-        
-    System.out.println("Fibonacci Series Upto " + n + ": ");
-    
-    while (firstTerm <= n) {
-      System.out.print(firstTerm + ", ");
+public class Fibonacci 
+{
+    public static void Fib(int n) 
+    {
+        int a = 0, b = 1, c;
 
-      int nextTerm = firstTerm + secondTerm;
-      firstTerm = secondTerm;
-      secondTerm = nextTerm;
-
+        if (n == 1)
+            System.out.println(a);
+        else if (n == 2)
+            System.out.println(a + " " + b);
+        else
+        {
+            System.out.print(a + " " + b + " ");
+            for(int i = 2; i < n; i++)
+            {
+                c = a + b;
+                System.out.print(c + " ");
+                a = b;
+                b = c;
+            }
+        }    
     }
-  }
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        
+        while(true)
+        {
+            System.out.print("Enter a number: ");
+            n = sc.nextInt();
+            
+            if (n <= 0)
+                System.out.println("Invalid input!!");
+            else
+                break;
+        }
+        
+        System.out.println("Fibonacci Series Upto " + n + " terms: ");
+
+        Fib(n);
+        sc.close();
+    }
 }
