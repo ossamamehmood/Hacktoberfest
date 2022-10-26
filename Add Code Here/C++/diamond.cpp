1cd462280f6654c34f4afe06d36551e44f95e0b4
ossamamehmood <ossamamehmood110@gmail.com>
2022-10-27 01:33:13 +0500

    Hacktoberfest

--- Add Code Here/C++/diamond.cpp
+++ Add Code Here/C++/diamond.cpp
@@ -1,49 +1,30 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
-	int n=9,i,x,b,y;
-	for(i=1;i<=n;i++)
-	{
-		if(i%2==0)
-		{
-			continue;
-		}
-		else
-		{
-			x=(n-i)/2;
-			y=i;
-			while(x!=0)
-			{
-				printf(" ");
-				x--;
-			}
-		}
-		for(b=0;b<y;b++)
-			{
-				printf("R");
-		    }	
-		    printf("\n");
-    }
-	for(i=9;i>=1;i--)
-	{
-		if(i%2==0)
-		{
-			continue;
-		}
-		else
-		{
-			x=(n-i)/2;
-			y=i;
-			while(x!=0)
-			{
-				printf(" ");
-				x--;
-			}
-		}
-		for(b=0;b<y;b++)
-			{
-			printf("G");
-		    }	
-		    printf("\n");
-	}
+        int n, c, k, space = 1;
+        cout<<"\n Enter Number of Rows : ";
+        cin>>n;
+        space = n - 1;
+        for(k = 1; k<=n; k++)
+        {
+                for (c = 1; c<=space; c++)
+                        cout<<" ";
+                space--;
+                for (c = 1; c<= 2*k-1; c++)
+                        cout<<"*";
+                cout<<"\n";
+        }
+        space = 1;
+        for(k = 1; k<= n - 1; k++)
+        {
+                for (c = 1; c<= space; c++)
+                        cout<<" ";
+                space++;
+                for (c = 1 ; c<= 2*(n-k)-1; c++)
+                        cout<<"*";
+                cout<<"\n";
+        }
+        return 0;
 }
+
