5641f3964df15ca81118fd2a6df9ec41140a40f3
Nitin Dixit <55051669+nitindxt@users.noreply.github.com>
2022-10-27 06:27:47 +0530

    Create FindVowels.js

---
+++ FindVowels.js
@@ -0,0 +1,11 @@
+# find vowels
+const findVowels = str => {
+  let count = 0
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  for(let char of str.toLowerCase()) {
+    if(vowels.includes(char)) {
+      count++
+    }
+  }
+  return count
+}
