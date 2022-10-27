154b8cd743a4d20ef7568d450262392953b199e1
Amarjeet Prajapati <63331731+Amarjeet2753@users.noreply.github.com>
2022-10-27 16:03:11 +0530

    Create floorInSortedArray.cpp
    This program return the index of floor of given number in an sorted array.
    If floor does not exist of that number in the array it will return -1.

---
+++ floorInSortedArray.cpp
@@ -0,0 +1,99 @@
+/*
+
+Question : Given a sorted array arr[] of size N without duplicates, and given a value x. Floor of x is defined as the largest element K in arr[] such that K is smaller than or equal to x. Find the index of K(0-based indexing).
+
+
+* First enter the number of test cases 
+
+Example 1:
+
+Input:
+N = 7, x = 0 
+arr[] = {1,2,8,10,11,12,19}
+Output: -1
+Explanation: No element less 
+than 0 is found. So output 
+is "-1".
+
+Example 2:
+
+Input:
+N = 7, x = 5 
+arr[] = {1,2,8,10,11,12,19}
+Output: 1
+Explanation: Largest Number less than 5 is
+2 (i.e K = 2), whose index is 1(0-based 
+indexing).
+
+
+Expected Time Complexity: O(log N).
+Expected Auxiliary Space: O(1).
+
+
+*/
+
+
+
+//{ Driver Code Starts
+#include<iostream>
+#include<algorithm>
+#include<climits>
+#include<vector>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        // Your code here
+          int low=0,high=n-1;
+        
+        while(low<=high){
+        int mid=(low+high)/2;
+        
+        if(v[mid]>x) high=mid-1;
+        else if(mid==n-1 || (v[mid]<=x && v[mid+1]>x)) return mid;
+        else
+         low=mid+1;
+        
+        
+        }
+        return -1;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    long long n;
+	    cin >> n;
+	    long long x;
+	    cin >> x;
+	    
+	    vector<long long> v;
+	    
+	    for(long long i = 0;i<n;i++){
+	        long long temp;
+	        cin >> temp;
+	        v.push_back(temp);
+	    }
+	    Solution obj;
+	    cout << obj.findFloor(v, n, x) << endl;
+	   
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
