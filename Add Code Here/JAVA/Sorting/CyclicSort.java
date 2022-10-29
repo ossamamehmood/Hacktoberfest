5a5a986a2df50f2b8cf85f2b306c17eddce007b1
KajalSingh9 <113881724+KajalSingh9@users.noreply.github.com>
2022-10-29 21:51:46 +0530

    Create CyclicSort.java

---
+++ Add Code Here/JAVA/Sorting/CyclicSort.java
@@ -0,0 +1,28 @@
+package com.rishabh.cyclic;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cyclicSort(int[] arr){
+        int i = 0;
+        while(i < arr.length){
+            //arr[arr[i] - 1] = arr[correct index]
+            if(arr[i] != arr[arr[i] - 1]){
+                swapArray(arr,i,arr[i] - 1);
+            }else {
+                i++;
+            }
+        }
+    }
+    static void swapArray(int[] arr,int first,int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
