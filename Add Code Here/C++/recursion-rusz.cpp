0d93462c536c745ec86a824cb1942d1fff56d0eb
rusz2 <116600962+rusz2@users.noreply.github.com>
2022-10-30 00:28:34 +0700

    Create recursion-rusz.cpp

---
+++ Add Code Here/C++/recursion-rusz.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Declare the function
+int Power(int, int);
+
+int main()
+{
+    int x = 2, n = 3, result;
+
+    //Call the power function to calculate x to the power n
+    result = Power(x, n);
+    cout << x << " to the power " << n << " is " << result < 0)
+    //Calculate power using recursive function
+    return (base*Power(base, powerVal-1));
+    else
+    return 1;
+}
