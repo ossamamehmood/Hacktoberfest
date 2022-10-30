b8d211b12b205e1539e4cdc33a01f1aa0fbbd834
danny <danishmultani2001@gmail.com>
2022-10-30 15:30:01 +0530

    Added LevelOrder Traversals.

---
+++ Add Code Here/JAVA/tree_traversal_level.java
@@ -0,0 +1,66 @@
+class Node {
+	int data;
+	Node left, right;
+	public Node(int item)
+	{
+		data = item;
+		left = right = null;
+	}
+}
+
+class BinaryTree {
+	// Root of the Binary Tree
+	Node root;
+
+	public BinaryTree() { root = null; }
+
+	void printLevelOrder()
+	{
+		int h = height(root);
+		int i;
+		for (i = 1; i <= h; i++)
+			printCurrentLevel(root, i);
+	}
+
+	int height(Node root)
+	{
+		if (root == null)
+			return 0;
+		else {
+			int lheight = height(root.left);
+			int rheight = height(root.right);
+
+			if (lheight > rheight)
+				return (lheight + 1);
+			else
+				return (rheight + 1);
+		}
+	}
+
+	/* Print nodes at the current level */
+	void printCurrentLevel(Node root, int level)
+	{
+		if (root == null)
+			return;
+		if (level == 1)
+			System.out.print(root.data + " ");
+		else if (level > 1) {
+			printCurrentLevel(root.left, level - 1);
+			printCurrentLevel(root.right, level - 1);
+		}
+	}
+
+	public static void main(String args[])
+	{
+		BinaryTree tree = new BinaryTree();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+
+		System.out.println("Level order traversal of"
+						+ "binary tree is ");
+		tree.printLevelOrder();
+	}
+}
