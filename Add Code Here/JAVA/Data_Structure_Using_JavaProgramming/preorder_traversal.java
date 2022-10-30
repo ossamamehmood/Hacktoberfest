893dddeada5a6eb87d3ca6e34e7b0b4d1d41bb5a
danny <danishmultani2001@gmail.com>
2022-10-30 14:36:37 +0530

    Added Preorder Traversals.

---
+++ Add Code Here/JAVA/Data_Structure_Using_JavaProgramming/preorder_traversal.java
@@ -0,0 +1,52 @@
+class Node {
+	int key;
+	Node left, right;
+
+	public Node(int item)
+	{
+		key = item;
+		left = right = null;
+	}
+}
+
+class BinaryTree {
+	// Root of Binary Tree
+	Node root;
+
+	BinaryTree() { root = null; }
+
+	/* Given a binary tree, print its nodes in preorder*/
+	void printPreorder(Node node)
+	{
+		if (node == null)
+			return;
+
+		/* first print data of node */
+		System.out.print(node.key + " ");
+
+		/* then recur on left subtree */
+		printPreorder(node.left);
+
+		/* now recur on right subtree */
+		printPreorder(node.right);
+	}
+
+	// Wrappers over above recursive functions
+	void printPreorder() { printPreorder(root); }
+
+	// Driver code
+	public static void main(String[] args)
+	{
+		BinaryTree tree = new BinaryTree();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+
+		// Function call
+		System.out.println(
+			"Preorder traversal of binary tree is ");
+		tree.printPreorder();
+	}
+}
