d416bd8ec52d3ca976eadc3a80b67857eaedf406
danny <danishmultani2001@gmail.com>
2022-10-30 15:26:29 +0530

    Added PostOrder Traversals.

---
+++ Add Code Here/JAVA/tree_traversal_postorder.java
@@ -0,0 +1,48 @@
+class Node {
+	int key;
+	Node left, right;
+
+	public Node(int item)
+	{
+		key = item;
+		left = right = null;
+	}
+}
+
+class BinaryTree {
+	// Root of Binary Tree
+	Node root;
+
+	BinaryTree() { root = null; }
+
+	void printPostorder(Node node)
+	{
+		if (node == null)
+			return;
+
+		// first recur on left subtree
+		printPostorder(node.left);
+
+		// then recur on right subtree
+		printPostorder(node.right);
+
+		// now deal with the node
+		System.out.print(node.key + " ");
+	}
+
+	void printPostorder() { printPostorder(root); }
+
+	public static void main(String[] args)
+	{
+		BinaryTree tree = new BinaryTree();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+
+		System.out.println(
+			"\nPostorder traversal of binary tree is ");
+		tree.printPostorder();
+	}
+}
