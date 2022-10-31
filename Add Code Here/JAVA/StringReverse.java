e7a95bdd1fdf2486e79afdc8918f1e9a80f75785
Rishabh000 <56599651+Rishabh000@users.noreply.github.com>
2022-10-31 12:31:36 +0530

    reverse a string in java

---
+++ Add Code Here/JAVA/StringReverse.java
@@ -0,0 +1,14 @@
+public class StringReverse{
+
+public static void main(String[] args){
+
+String name="Weather is Wonderful";
+int l=name.length();
+
+for(int i=l-1;i>=0;i--){
+
+System.out.print(name.charAt(i));
+}
+}
+
+}
