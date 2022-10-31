e7df4e28f6e9fbefe4648f9271788cbcf97dd3b6
Vaibhavpan02 <89473295+Vaibhavpan02@users.noreply.github.com>
2022-10-31 21:25:43 +0530

    Create binary_search.java

---
+++ binary_search.java
@@ -0,0 +1,26 @@
+class BinarySearchExample1{  
+    public static int binarySearch(int arr[], int first, int last, int key){  
+        if (last>=first){  
+            int mid = first + (last - first)/2;  
+            if (arr[mid] == key){  
+            return mid;  
+            }  
+            if (arr[mid] > key){  
+            return binarySearch(arr, first, mid-1, key);//search in left subarray  
+            }else{  
+            return binarySearch(arr, mid+1, last, key);//search in right subarray  
+            }  
+        }  
+        return -1;  
+    }  
+    public static void main(String args[]){  
+        int arr[] = {10,20,30,40,50};  
+        int key = 30;  
+        int last=arr.length-1;  
+        int result = binarySearch(arr,0,last,key);  
+        if (result == -1)  
+            System.out.println("Element is not found!");  
+        else  
+            System.out.println("Element is found at index: "+result);  
+    }  
+}  
