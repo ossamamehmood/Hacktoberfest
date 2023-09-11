73d53367a0259189feaddf52afb8b02adfd4b596
Anianonymous <hegdeanirudh2003@gmail.com>
2023-09-11 20:55:37 +0530

    added unique file

--- Add Code Here/.py
+++ Add Code Here/.py
@@ -1,1 +1,18 @@
+# url shortener
+import gradio as gr
+import pyshorteners
 
+#creating a function to shorten the url
+def shorten_url(url):
+    shortener = pyshorteners.Shortener()
+    short_url = shortener.tinyurl.short(url)
+    return short_url
+
+#call the function
+def url_shortener(url):
+    short_url = shorten_url(url)
+    return short_url
+
+#Creating an Interface to give inputs and display outputs
+iface = gr.Interface(fn=url_shortener, inputs="text", outputs="text", title="URL Shortener")
+iface.launch(share=True)
