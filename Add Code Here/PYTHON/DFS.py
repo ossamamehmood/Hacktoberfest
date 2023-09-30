d1331ec94a6d8ba1f2c595518f0a04947db4fd14
Niyati Soni <85472780+Niyati3031@users.noreply.github.com>
2023-09-30 19:09:35 +0530

    Update DFS.py
    Added driver code

--- Add Code Here/PYTHON/DFS.py
+++ Add Code Here/PYTHON/DFS.py
@@ -42,15 +42,16 @@ class Graph:
 
 
 # Driver code
-# Create a graph 
-g = Graph()
-g.addEdge(0, 1)
-g.addEdge(0, 2)
-g.addEdge(1, 2)
-g.addEdge(2, 0)
-g.addEdge(2, 3)
-g.addEdge(3, 3)
-
-print "Following is Depth First Traversal"
-g.DFS()
+if __name__ == '__main__':
+	# Create a graph 
+	g = Graph()
+	g.addEdge(0, 1)
+	g.addEdge(0, 2)
+	g.addEdge(1, 2)
+	g.addEdge(2, 0)
+	g.addEdge(2, 3)
+	g.addEdge(3, 3)
+	
+	print "Following is Depth First Traversal"
+	g.DFS()
 
