c5745859ebb94da369a8e97e63b8909834f7031d
Utsav Rai <utsavrai2115@gmail.com>
2023-10-02 01:07:16 +0530

    Easy

---
+++ Add Code Here/LeetCode - 30 Days Of JavaScript/1. Create Hello World Function.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+const f = createHelloWorld();
+f()
+
+/** Follow on Github : @utsxvrai  **/
