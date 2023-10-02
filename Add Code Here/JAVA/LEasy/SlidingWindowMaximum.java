08dd56cb7f38feb0da0b9f985e96a11efad9e6c6
gaurijoshi22 <gauri.joshi@cumminscollege.in>
2023-10-02 11:36:56 +0530

    Added code

---
+++ Add Code Here/JAVA/LEasy/SlidingWindowMaximum.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class SlidingWindowMaximum {
+    public static int[] maxSlidingWindow(int[] a, int k) {
+        int n = a.length;
+        int[] r = new int[n - k + 1];
+        int ri = 0;
+        // store index
+        Deque < Integer > q = new ArrayDeque < > ();
+        for (int i = 0; i < a.length; i++) {
+            // remove numbers out of range k
+            if (!q.isEmpty() && q.peek() == i - k) {
+                q.poll();
+            }
+            // remove smaller numbers in k range as they are useless
+            while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
+                q.pollLast();
+            }
+
+            q.offer(i);
+            if (i >= k - 1) {
+                r[ri++] = a[q.peek()];
+            }
+        }
+        return r;
+    }
+    public static void main(String args[]) {
+        int i, j, n, k = 3, x;
+        int arr[]={4,0,-1,3,5,3,6,8};
+        int ans[] = maxSlidingWindow(arr, k);
+        System.out.println("Maximum element in every " + k + " window ");
+        for (i = 0; i < ans.length; i++)
+            System.out.print(ans[i] + "  ");
+
+    }
+}
