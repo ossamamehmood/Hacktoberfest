76faf7a957caa56796bd9f6a3632c6db116e5830
jayprakash6547 <87560854+jayprakash6547@users.noreply.github.com>
2023-10-02 08:33:28 +0530

    Create Zigzag Conversion.cpp

---
+++ Add Code Here/C++ Leetcode Solutions/Zigzag Conversion.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        string ret;
+        int n = s.size();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                ret += s[j + i];
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+                    ret += s[j + cycleLen - i];
+            }
+        }
+        return ret;
+    }
+};
