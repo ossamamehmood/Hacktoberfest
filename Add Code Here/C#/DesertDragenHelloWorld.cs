1dc00e1412ec6ecbf80956a7aea6c34d2b7d5253
tushar-temp <114791681+tushar-temp@users.noreply.github.com>
2023-10-02 07:58:55 +0530

    Update DesertDragenHelloWorld.cs

--- Add Code Here/C#/DesertDragenHelloWorld.cs
+++ Add Code Here/C#/DesertDragenHelloWorld.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main()
     {
+         // Print a greeting message
         Console.WriteLine("DesertDragen says: Hello, World!");
+
+        // Prompt the user to enter some input
+        Console.Write("Enter something: ");
+        
+        // Read the user's input
+        string userInput = Console.ReadLine();
+        
+        // Print the input provided by the user
+        Console.WriteLine("You entered: " + userInput);
     }
 }
