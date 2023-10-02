6aec928851f52f98c7ea348f4ea37d880d2843b7
bhoomikarao22 <95897461+bhoomikarao22@users.noreply.github.com>
2023-10-02 14:26:45 +0530

    Update binary-search.c
    The updated code takes user input to perform binary search

--- Add Code Here/C/binary-search.c
+++ Add Code Here/C/binary-search.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 
 int binarySearch(int array[], int x, int low, int high) {
-  if (high >= low) {
-    int mid = low + (high - low) / 2;
+    while (high >= low) {
+        int mid = low + (high - low) / 2;
 
-    // If found at mid, then return it
-    if (array[mid] == x)
-      return mid;
+        // If found at mid, then return it
+        if (array[mid] == x)
+            return mid;
 
-    // Search the left half
-    if (array[mid] > x)
-      return binarySearch(array, x, low, mid - 1);
+        // Search the left half
+        if (array[mid] > x)
+            high = mid - 1;
+        else
+            low = mid + 1;  // Search the right half
+    }
 
-    // Search the right half
-    return binarySearch(array, x, mid + 1, high);
-  }
-
-  return -1;
+    return -1;
 }
 
 int main(void) {
-  int array[] = {3, 4, 5, 6, 7, 8, 9};
-  int n = sizeof(array) / sizeof(array[0]);
-  int x = 4;
-  int result = binarySearch(array, x, 0, n - 1);
-  if (result == -1)
-    printf("Not found");
-  else
-    printf("Element is found at index %d\n", result);
+    int n;
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    
+    int array[n];
+    
+    printf("Enter %d sorted integers:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &array[i]);
+    }
+    
+    int x;
+    printf("Enter the number to search for: ");
+    scanf("%d", &x);
+    
+    int result = binarySearch(array, x, 0, n - 1);
+    if (result == -1)
+        printf("Not found\n");
+    else
+        printf("Element is found at index %d\n", result);
+    return 0;
 }
+
