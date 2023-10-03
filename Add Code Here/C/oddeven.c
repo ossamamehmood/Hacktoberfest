3864950a7bd40b52f0a96440e7cdd765bc155082
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-10-04 02:08:24 +0530

    Update oddeven.c

--- Add Code Here/C/oddeven.c
+++ Add Code Here/C/oddeven.c
@@ -1,35 +1,24 @@
 #include<stdio.h>
+#include<conio.h>
 int oddeven(int num){
-        if(num%2==1)
-                return 1;
-        else
+        if(num%2==0){
                 return 0;
-}
-int main(){
-        int a,res;
-        char choice;
-    do {
-        printf("Enter any number to check odd or even: ");
-        if (scanf("%d", &a) != 1) {
-            printf("Invalid input. Please enter a valid integer.\n");
-            // Clear the input buffer to prevent an infinite loop
-            while (getchar() != '\n');
-            continue;
         }
+        else{
+                return 1;
+        }
+}
+void main(){
+        int number;
+        printf("Enter Number Which You Want To Check : ");
+        scanf("%d",&number);
 
-        res = oddeven(a);
+        int check = oddeven(number);
 
-        if (res == 1)
-            printf("The number is odd\n");
-        else
-            printf("The number is even\n");
-
-        printf("Do you want to check another number? (y/n): ");
-        while (getchar() != '\n'); // Clear the input buffer
-        scanf("%c", &choice);
-    } while (choice == 'y' || choice == 'Y');
-
-    printf("Exiting the program.\n");
-
-    return 0;
+        if(check==0){
+                printf("The Given Number Is Even");
+        }
+        else{
+                printf("The Given Number Is Odd");
+        }
 }
