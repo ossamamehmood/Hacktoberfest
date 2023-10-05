738b6f9ced423bc8047f7f375094973aee692ac4
camarota <joao.camarota@discente.ufg.br>
2023-10-05 12:06:57 -0300

    adding code calculate-pi

---
+++ Add Code Here/PYTHON/calculate-pi.py
@@ -0,0 +1,17 @@
+def calculate_pi(number):
+    
+    denominator = 1.0
+    operation = 1.0
+    pi = 0.0
+    
+    for _ in range(number):
+        pi += operation * (4.0 / denominator)
+        denominator += 2.0
+        operation *= -1.0
+    return pi
+
+
+if __name__ == "__main__":
+    n_terms = [10, 1000, 100000, 10000000]
+    for n in n_terms:
+        print(f"PI ({n}): {calculate_pi(n)}")
