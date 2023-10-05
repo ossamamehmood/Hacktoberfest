949c37c51286d185361396b0cf45307ac8d7ff5c
Rahul Tank <rahul.tank@espressif.com>
2023-10-05 13:37:32 +0530

    Add sample code to exhibit data type usage

---
+++ Add Code Here/C/print.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+int main(void)
+{
+   char i = 5 ;
+   
+   for(;i<500;i++)
+       printf("%d ", i);
+
+   return 0;
+}
---
+++ content/participant/rahul.md
@@ -0,0 +1,3 @@
+name: rahul
+institution/company: Espressif
+github:rahult-github
