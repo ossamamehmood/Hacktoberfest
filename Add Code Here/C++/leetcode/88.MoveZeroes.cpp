05af736ad637c9dbfeb9e151676824f4c1696738
Vasuaggarwal0509 <127047852+Vasuaggarwal0509@users.noreply.github.com>
2023-10-06 04:10:53 +0530

    Update 88.MoveZeroes.cpp
    very short code for leetcode problem #283 .

--- Add Code Here/C++/leetcode/88.MoveZeroes.cpp
+++ Add Code Here/C++/leetcode/88.MoveZeroes.cpp
@@ -1,19 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-void moveZeroes(vector<int>& nums) {
-    for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
-        if (nums[cur] != 0) {
-            swap(nums[lastNonZeroFoundAt++], nums[cur]);
-        }
-    }
-}
-
-int main(){
-    vector<int> v = {1,0};
-    moveZeroes(v);
-    for(int i:v) cout<<i<<" "; 
-
-
-    return 0;
-}
+#include <vector>
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+           int i=0;int j=0;
+          while(j<nums.size())
+           { if(nums[j]!=0)
+           {
+           swap(nums[j],nums[i]);
+           i++;
+       }
+       j++;
+           }
+    }};
