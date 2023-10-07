103515a2fbf928c21a5408d9095d25236f9fe0de
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2023-10-07 18:14:08 +0530

    Adding MergeTwoSortedLists

---
+++ Add Code Here/JAVA/MergeTwoSortedLists.java
@@ -0,0 +1,53 @@
+class ListNode {
+    int val;
+    ListNode next;
+    
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class MergeTwoSortedLists {
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        
+        if (l1 != null) {
+            current.next = l1;
+        } else {
+            current.next = l2;
+        }
+        
+        return dummy.next;
+    }
+    
+    public static void main(String[] args) {
+        // Creating two sorted linked lists
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        
+        // Merging and printing the result
+        ListNode mergedList = mergeTwoLists(l1, l2);
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " ");
+            mergedList = mergedList.next;
+        }
+    }
+}
