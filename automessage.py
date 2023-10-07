9ff2f5332f3809672ba25404bc5c37ec5c624aea
Ossama Mehmood 샘 <ossamamehmood110@gmail.com>
2023-10-08 00:11:09 +0500

    Delete automessage.py

--- automessage.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time
-
-time.sleep(3)
-for i in range(200):
-    pyautogui.click(y=980, x=730, duration=0.4)
-    pyautogui.write("Your Message")
-    pyautogui.press('enter')
