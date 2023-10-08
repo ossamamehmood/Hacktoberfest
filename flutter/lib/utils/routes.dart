25707dfb6c8547fd170bb2c6541fa624640b11b4
Ossama Mehmood 샘 <ossamamehmood110@gmail.com>
2023-10-09 00:29:01 +0500

    Delete flutter directory

--- flutter/lib/utils/routes.dart
+++
@@ -1,7 +0,0 @@
-class MyRoutes{
-  static String loginRoute = "/login";
-  static String homeRoute = "/home";
-  static String homeDetailsRoute = "/detail";
-  static String shopPageRoute = "/shop";
-  static String cartRoute = "/cart";
-}
