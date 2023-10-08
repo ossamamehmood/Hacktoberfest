b25018eccca701e28a0d7dbfbddd4d89b141dffe
Harsh V <72182555+Harsh-Vipin@users.noreply.github.com>
2023-10-08 11:09:57 +0530

    Create binarySearch.cpp

---
+++ Add Code Here/CPP/binarySearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+int binarySearch(const std::vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;  // Target found, return the index
+        } else if (arr[mid] < target) {
+            left = mid + 1;  // Target is in the right half
+        } else {
+            right = mid - 1;  // Target is in the left half
+        }
+    }
+
+    return -1;  // Target not found in the array
+}
+
+int main() {
+    std::vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int target = 5;
+
+    int result = binarySearch(arr, target);
+
+    if (result != -1) {
+        std::cout << "Element " << target << " found at index " << result << std::endl;
+    } else {
+        std::cout << "Element " << target << " not found in the array" << std::endl;
+    }
+
+    return 0;
+}
