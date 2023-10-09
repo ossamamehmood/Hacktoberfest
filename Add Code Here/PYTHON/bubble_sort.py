348711b1f2e379a02bd6fb55881e288ee1509d75
UdeeshaRukshan <IT21807480@my.sliit.lk>
2023-10-09 17:27:20 +0530

    2nd part of the code

--- Add Code Here/PYTHON/bubble_sort.py
+++ Add Code Here/PYTHON/bubble_sort.py
@@ -11,15 +11,15 @@ def bubble_sort(arr):
 
 arr = []
 
-for i in range(1, 6):
-    n = int(input(f"Enter Number between 10 and 20 {i}:"))
-    if n > 10 and n < 20:
-        arr.append(n)
-    else:
-        print("Enter a valid number between 10 and 20.")
-        n = int(input(f"Enter Number between 10 and 20 {i}:"))
-    i += 1
+# for i in range(1, 6):
+#     n = int(input(f"Enter Number between 10 and 20 {i}:"))
+#     if n > 10 and n < 20:
+#         arr.append(n)
+#     else:
+#         print("Enter a valid number between 10 and 20.")
+#         n = int(input(f"Enter Number between 10 and 20 {i}:"))
+#     i += 1
 
-bubble_sort(arr)
+# bubble_sort(arr)
 
-print("Sorted array:", arr)
+# print("Sorted array:", arr)
