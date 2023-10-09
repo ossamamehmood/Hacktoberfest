661f4f0b42971b7299abe61c9e00905b51e14752
Komal Kaur <komalkaurstaymenity@gmail.com>
2023-10-08 17:40:01 -0700

    added reverseString in js

---
+++ Add Code Here/Javascript/ReverseAString.js
@@ -0,0 +1,11 @@
+//Reverse a String
+
+function reverseString(str) {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
+}
+
+reverseString("hello");
