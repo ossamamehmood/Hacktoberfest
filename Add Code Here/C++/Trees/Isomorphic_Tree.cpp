bcb789f77a2b792ac8b2912e932626b68132ffd9
Swaroop Raghavendra Anegundi <83859431+swaroop-2021@users.noreply.github.com>
2023-10-11 01:41:25 +0530

    Create Isomorphic_Tree.cpp

---
+++ Add Code Here/C++/Trees/Isomorphic_Tree.cpp
@@ -0,0 +1,113 @@
+// https://practice.geeksforgeeks.org/problems/check-if-tree-is-isomorphic/1
+// Return True if the given trees are isomotphic. Else return False.
+
+#include<bits/stdc++.h>
+using namespace std;
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+class Solution{
+  public:
+    
+    bool isIsomorphic(Node *root1,Node *root2)
+    {
+        if(!root1 || !root2)
+            return root1==root2;
+        if(root1->data!=root2->data)
+            return false;
+        
+        bool a=isIsomorphic(root1->left,root2->left)||isIsomorphic(root1->left,root2->right);
+        bool b=isIsomorphic(root1->right,root2->left)||isIsomorphic(root1->right,root2->right);
+        return a&&b;
+    }
+};
+
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s1,s2;
+		getline(cin,s1);
+		getline(cin,s2);
+		Node* root1 = buildTree(s1);
+		Node* root2 = buildTree(s2);
+		Solution obj;
+		if(obj.isIsomorphic(root1,root2))
+		    cout<<"Yes"<<endl;
+		else
+		    cout<<"No"<<endl;
+    }
+    return 0;
+}
