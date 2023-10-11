198c2c9eb203191f9d10ac04198fb0459aaf47e8
Bestmymint <118463140+Bestmymint@users.noreply.github.com>
2023-10-12 00:02:47 +0700

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,155 @@
+Below is the tic tac toe Python program. Enjoy Playing!!!
+The First Player's name
+Please mention your name: john
+The Second Player's name
+Please mention your name:  jiya
+--------------------------------
+The SCOREBOARD for TIC TAC TOE PYTHON GAME       
+--------------------------------
+    john 	     0
+    jiya 	     0
+--------------------------------
+
+john , you get the chance to make the choice for the series of the Tic tac toe Python game:
+Please press 1 for X
+Please press 2 for O
+Please press 3 for Exit
+1
+using the data structure to store the filled positions denoted by X and O 
+
+
+	      |      |
+	      |      |   
+	______|______|______
+	      |      |
+	      |      |   
+	______|______|______
+	      |      |
+	      |      |   
+	      |      |
+
+
+To handle the input from the player using the Try-Exception block for the 'CHANCE' input 
+The player  X  turn. Now you need to choose your block: 1
+Below is the sanity check for the 'CHANCE' input
+To check if the block in the grid is not filled up already 
+
+
+	      |      |
+	    X |      |   
+	______|______|______
+	      |      |
+	      |      |   
+	______|______|______
+	      |      |
+	      |      |   
+	      |      |
+
+
+To handle the input from the player using the Try-Exception block for the 'CHANCE' input 
+The player  O  turn. Now you need to choose your block: 4
+Below is the sanity check for the 'CHANCE' input
+To check if the block in the grid is not filled up already 
+                
+	    X |      |   
+	______|______|______
+	      |      |
+	   O  |      |   
+	______|______|______
+	      |      |
+	      |      |   
+	      |      |
+
+
+To handle the input from the player using the Try-Exception block for the 'CHANCE' input 
+The player  X  turn. Now you need to choose your block : 3
+This is an Invalid Input!!! Please try again!
+
+
+	      |      |
+	    X |      |   
+	______|______|______
+	      |      |
+	   O  |      |   
+	______|______|______
+	      |      |
+	      |      |   
+	      |      |
+
+
+To handle the input from the player using the Try-Exception block for the 'CHANCE' input 
+The player  X  turn. Now you need to choose your block: 5
+Below is the sanity check for the 'CHANCE' input
+To check if the block in the grid is not filled up already 
+
+
+	      |      |
+	    X |      |   
+	______|______|______
+	      |      |
+	   O  |  X   |   
+	______|______|______
+	      |      |
+	      |      |   
+	      |      |
+
+
+To handle the input from the player using the Try-Exception block for the 'CHANCE' input 
+The player  O  turn. Now you need to choose your block: 6
+Below is the sanity check for the 'CHANCE' input
+To check if the block in the grid is not filled up already 
+
+
+	      |      |
+	    X |      |   
+	______|______|______
+	      |      |
+	   O  |  X   |  O
+	______|______|______
+	      |      |
+	      |      |   
+	      |      |
+
+
+To handle the input from the player using the Try-Exception block for the 'CHANCE' input 
+The player  X  turn. Now you need to choose your block: 9
+Below is the sanity check for the 'CHANCE' input
+To check if the block in the grid is not filled up already 
+
+
+	      |      |
+	    X |      |   
+	______|______|______
+	      |      |
+	   O  |  X   |  O
+	______|______|______
+	      |      |
+	      |      |  X
+	      |      |
+
+
+Hurray! You nailed it!  X  has won the tic tac toe Python game!
+
+
+--------------------------------
+The SCOREBOARD for TIC TAC TOE PYTHON GAME       
+--------------------------------
+    john 	     1
+    jiya 	     0
+--------------------------------
+
+jiya , you get the chance to make the choice for the series of the Tic tac toe Python game:
+Please press 1 for X
+Please press 2 for O
+Please press 3 for Exit
+3
+Thanks for playing the game!!!
+The final scores are
+--------------------------------
+The SCOREBOARD for TIC TAC TOE PYTHON GAME       
+--------------------------------
+    john 	     1
+    jiya 	     0
+--------------------------------
+
+
