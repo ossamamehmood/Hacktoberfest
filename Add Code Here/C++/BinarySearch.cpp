075f005ac1e1d4d9dbcb11503ba7648b468a3be7
Md. Bazlur Rahman Likhon <105287746+brlikhon@users.noreply.github.com>
2023-10-15 21:58:30 +0600

    Create BinarySearch.cpp

---
+++ Add Code Here/C++/BinarySearch.cpp
@@ -0,0 +1,11 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
