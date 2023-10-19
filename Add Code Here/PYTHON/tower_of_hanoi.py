8838fdcdc34747f4566042052acf81d4d6835be8
Kavinkumar-2004 <146543755+Kavinkumar-2004@users.noreply.github.com>
2023-10-19 22:39:50 +0530

    Add files via upload

---
+++ Add Code Here/PYTHON/tower_of_hanoi.py
@@ -0,0 +1,11 @@
+def tower_of_hanoi(n, source, auxiliary, target):
+    if n == 1:
+        print(f"Move disk 1 from {source} to {target}")
+        return
+    tower_of_hanoi(n - 1, source, target, auxiliary)
+    print(f"Move disk {n} from {source} to {target}")
+    tower_of_hanoi(n - 1, auxiliary, source, target)
+
+
+n = 3 
+tower_of_hanoi(n, 'A', 'B', 'C')
