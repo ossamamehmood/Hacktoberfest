034782db1f6f2f5839ce40530dde6dc942947a0b
Koushambha Das <122219149+KOUSHAMBHA15IT@users.noreply.github.com>
2023-10-27 20:15:54 +0530

    Create Track.py

---
+++ Add Code Here/PYTHON/Track.py
@@ -0,0 +1,32 @@
+# Python program to detect faces
+import cv2
+
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+video_capture = cv2.VideoCapture(0)
+def detect_bounding_box(vid):
+    gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    return faces
+while True:
+
+    result, video_frame = video_capture.read()  # read frames from the video
+    if result is False:
+        break  # terminate the loop if the frame is not read successfully
+
+    faces = detect_bounding_box(
+        video_frame
+    )  # apply the function we created to the video frame
+
+    cv2.imshow(
+        "My Face Detection Project", video_frame
+    )  # display the processed frame in a window named "My Face Detection Project"
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
