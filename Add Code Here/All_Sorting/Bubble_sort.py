206ba7b1c931b37b669ce44eb8c1dc25f6462711
sulaemanahmed <132727787+sulaemanahmed@users.noreply.github.com>
2023-10-31 23:29:38 -0500

    Update Bubble_sort.py
    Extend Bubble Sort program to accept user input for an array of elements and sort them.

--- Add Code Here/All_Sorting/Bubble_sort.py
+++ Add Code Here/All_Sorting/Bubble_sort.py
@@ -1,35 +1,39 @@
-# Python program for implementation of Bubble Sort
-
 def bubbleSort(arr):
-	n = len(arr)
-	# optimize code, so if the array is already sorted, it doesn't need
-	# to go through the entire process
-	swapped = False
-	# Traverse through all array elements
-	for i in range(n-1):
-		# range(n) also work but outer loop will
-		# repeat one time more than needed.
-		# Last i elements are already in place
-		for j in range(0, n-i-1):
+    n = len(arr)
+    # optimize code, so if the array is already sorted, it doesn't need
+    # to go through the entire process
+    swapped = False
+    # Traverse through all array elements
+    for i in range(n - 1):
+        # range(n) also works but the outer loop will
+        # repeat one time more than needed.
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-			# traverse the array from 0 to n-i-1
-			# Swap if the element found is greater
-			# than the next element
-			if arr[j] > arr[j + 1]:
-				swapped = True
-				arr[j], arr[j + 1] = arr[j + 1], arr[j]
-		
-		if not swapped:
-			# if we haven't needed to make a single swap, we 
-			# can just exit the main loop.
-			return
+        if not swapped:
+            # if we haven't needed to make a single swap, we
+            # can just exit the main loop.
+            return
 
+def main():
+    n = int(input("Enter the number of elements: "))
+    arr = []
+    for i in range(n):
+        element = int(input(f"Enter element {i+1}: "))
+        arr.append(element)
 
-# Driver code to test above
-arr = [64, 34, 25, 12, 22, 11, 90]
+    bubbleSort(arr)
 
-bubbleSort(arr)
+    print("Sorted array is:")
+    for i in range(len(arr)):
+        print("%d" % arr[i], end=" ")
 
-print("Sorted array is:")
-for i in range(len(arr)):
-	print("% d" % arr[i], end=" ")
+if __name__ == "__main__":
+    main()
+
