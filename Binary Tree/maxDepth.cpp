4520ca4cb8fe0ee9e3528386c3d062e6b5c86a98
sulaemanahmed <132727787+sulaemanahmed@users.noreply.github.com>
2023-10-31 23:04:24 -0500

    Update maxDepth.cpp
    Added code to create a binary tree, add nodes, and calculate the maximum depth of the tree.

--- Binary Tree/maxDepth.cpp
+++ Binary Tree/maxDepth.cpp
@@ -16,14 +16,22 @@ struct Node
 
 int maxDepth(Node *root)
 {
-    if(root==NULL)
-    return 0;
-    
+    if (root == NULL)
+        return 0;
+
     return max(maxDepth(root->left), maxDepth(root->right)) + 1;
 }
 
 int main()
 {
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    int depth = maxDepth(root);
+    cout << "Maximum depth of the binary tree: " << depth << endl;
 
     return 0;
 }
