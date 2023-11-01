111d81cabc550eb5dc9c85b0a3e8677603f06ebc
sulaemanahmed <132727787+sulaemanahmed@users.noreply.github.com>
2023-10-31 23:09:01 -0500

    Update calculator.java
    Added extended functionality to the calculator program, allowing users to perform multiple calculations until they choose to exit. The updated code handles division by zero and modulo by zero cases, enhancing the user experience.

--- calculator.java
+++ calculator.java
@@ -1,34 +1,60 @@
 import java.util.*;
 
-public class calculator{
-    public static void main(String args[]){
+public class Calculator {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        boolean continueCalculating = true;
 
-        System.out.println("Enter a : ");
-        int a = sc.nextInt();
-        System.out.println("Enter b : ");
-        int b = sc.nextInt();
-        System.out.println("Enter operator : ");
+        while (continueCalculating) {
+            System.out.println("Enter a: ");
+            int a = sc.nextInt();
+            System.out.println("Enter b: ");
+            int b = sc.nextInt();
+            System.out.println("Enter operator: ");
+            char operator = sc.next().charAt(0);
 
-        char operator = sc.next().charAt(0);    
+            int result = 0;
+            switch (operator) {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '/':
+                    if (b != 0) {
+                        result = a / b;
+                    } else {
+                        System.out.println("Division by zero is not allowed.");
+                        continue;
+                    }
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '%':
+                    if (b != 0) {
+                        result = a % b;
+                    } else {
+                        System.out.println("Modulo by zero is not allowed.");
+                        continue;
+                    }
+                    break;
+                default:
+                    System.out.println("Wrong operator");
+                    continue;
+            }
 
+            System.out.println("Result: " + result);
 
-        switch(operator){
-
-            case '+' : System.out.println(a+b);
-                        break;
-            case '-' : System.out.println(a-b);
-                        break;
-            case '/' : System.out.println(a/b);
-                        break;            
-            case '*' : System.out.println(a*b);
-                        break;
-            case '%' : System.out.println(a%b);
-                        break;
-            default :  System.out.println("wrong operator");            
-
+            // Ask if the user wants to continue
+            System.out.println("Do you want to perform another calculation? (Y/N)");
+            char choice = sc.next().charAt(0);
+            if (choice != 'Y' && choice != 'y') {
+                continueCalculating = false;
+            }
         }
-        sc.close();      
 
-    }    
-}   
+        sc.close();
+    }
+}
